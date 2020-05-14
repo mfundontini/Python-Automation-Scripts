928282dc394b5d8f9f53e830d024c2c1e043a3ae
MfundoNtini <mfundontini@gmail.com>
2020-05-14 03:39:41 +0200

    TEST: Testing out pattern matching and parsers.

--- atomizer/tests/features/mvp.feature
+++ atomizer/tests/features/mvp.feature
@@ -3,5 +3,15 @@ Feature: Test if behave is playing nicely with other kids
 
   Scenario: Check if everything runs
      Given we have behave "installed"
-      When we implement test number "8"
+      When we implement test number "9"
       Then behave will test it for us!
+
+  Scenario: Test pattern matching
+   Given we have behave "installed"
+    When we implement test number "x"
+    Then behave will test it for us!
+
+  Scenario: Test switching parsers
+   Given we have behave "installed"
+    When we run test number "6"
+    Then behave will test it for us!
--- atomizer/tests/features/steps/steps_implementation.py
+++ atomizer/tests/features/steps/steps_implementation.py
@@ -1,14 +1,35 @@
-from behave import *
+from behave import given, then, when, use_step_matcher
 
 
-@given('we have behave "{name}"')  # noqa
+use_step_matcher("re")
+
+@given('we have behave "(?P<name>\w+)"')  # noqa
 def step_impl(context, name):  # noqa
     print(name)
 
 
-@when('we implement test number "{numeral:d}"')  # noqa
+@when('we implement test number "(?P<numeral>\d+)"')  # noqa
 def step_impl(context, numeral):  # noqa
-    assert type(numeral) is int
+    try:
+        numeral = int(numeral)
+        print(numeral)
+    except ValueError:
+        context.failed = True
+
+
+@when('we implement test number "(?P<word>\w+)"')  # noqa
+def step_impl(context, word):  # noqa
+    print(word)
+    assert type(word) is str
+
+
+use_step_matcher("parse")
+
+
+@when('we run test number "{value:d}"')  # noqa
+def step_impl(context, value):  # noqa
+    print(value)
+    assert type(value) is int
 
 
 @then('behave will test it for us!')  # noqa
