037f7a6d20cd3d97779e2de88c9dd855ab9b72d1
MfundoNtini <mfundontini@gmail.com>
2020-05-14 02:44:17 +0200

    TEST: Flake8 fixes.

--- atomizer/tests/features/steps/steps_implementation.py
+++ atomizer/tests/features/steps/steps_implementation.py
@@ -1,16 +1,16 @@
 from behave import *
 
 
-@given('we have behave "{name}"')
-def step_impl(context, name):
+@given('we have behave "{name}"')  # noqa
+def step_impl(context, name):  # noqa
     print(name)
 
 
-@when('we implement test number "{numeral:d}"')
-def step_impl(context, numeral):
+@when('we implement test number "{numeral:d}"')  # noqa
+def step_impl(context, numeral):  # noqa
     assert type(numeral) is int
 
 
-@then('behave will test it for us!')
-def step_impl(context):
+@then('behave will test it for us!')  # noqa
+def step_impl(context):  # noqa
     assert context.failed is False
